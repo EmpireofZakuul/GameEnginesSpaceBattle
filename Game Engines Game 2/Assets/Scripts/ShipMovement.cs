aff017d8845a62a6d69a0476bcb26156f6df1333
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2021-03-01 17:32:23 +0000

    added steering behaviour script and waypoint script

---
+++ Game Engines Game 2/Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+    public Vector3 shipVelocity;
+    public float shipSpeed;
+    public Vector3 shipAcceleration;
+    public Vector3 shipForce;
+    public float maxSpeed = 10;
+    public float maxForce = 20;
+
+    public float shipsMass = 1;
+
+   // public bool seekEnabled = true;
+   // public Transform seekTargetTransform;
+   // public Vector3 seekTarget;
+
+    public float shipSlowingDistance = 80;
+    public WayPoints points;
+    public bool pathFollowingEnabled = false;
+    public float waypointAmount = 3;
+    // Banking
+    public float shipBanking = 0.1f;
+
+    public float damping = 0.1f;
+  
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawLine(transform.position, transform.position + shipVelocity);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + shipAcceleration);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + shipForce * 10);
+
+    }
+
+
+    public Vector3 PathFollow()
+    {
+        Vector3 nextWaypoint = points.NextPoint();
+        if (!points.loopedWaypoint && points.LastPoint())
+        {
+            return Arrive(nextWaypoint);
+        }
+        else
+        {
+            if (Vector3.Distance(transform.position, nextWaypoint) < waypointAmount)
+            {
+                points.MoveToNext();
+            }
+            return Seek(nextWaypoint);
+        }
+    }
+
+    public Vector3 Seek(Vector3 target)
+    {
+        Vector3 toTarget = target - transform.position;
+        Vector3 desired = toTarget.normalized * maxSpeed;
+
+        return (desired - shipVelocity);
+    }
+
+    public Vector3 Arrive(Vector3 target)
+    {
+        Vector3 toTarget = target - transform.position;
+        float distance = toTarget.magnitude;
+        float ramped = (distance / shipSlowingDistance) * maxSpeed;
+        float clamped = Mathf.Min(ramped, maxSpeed);
+        Vector3 desired = (toTarget / distance) * clamped;
+
+        return desired - shipVelocity;
+    }
+
+    public Vector3 CalculateForce()
+    {
+        Vector3 d = Vector3.zero;
+        /*if (seekEnabled)
+        {
+            if (seekTargetTransform != null)
+            {
+                seekTarget = seekTargetTransform.position;
+            }
+            f += Seek(seekTarget);
+        }
+        */
+       
+
+        if (pathFollowingEnabled)
+        {
+            d += PathFollow();
+        }
+
+        return d;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shipForce = CalculateForce();
+        shipAcceleration = shipForce / shipsMass;
+        shipVelocity = shipVelocity + shipAcceleration * Time.deltaTime;
+        transform.position = transform.position + shipVelocity * Time.deltaTime;
+        shipSpeed = shipVelocity.magnitude;
+        if (shipSpeed > 1)
+        {
+            Vector3 tempUp = Vector3.Lerp(transform.up, Vector3.up + (shipAcceleration * shipBanking), Time.deltaTime * 3.0f);
+            transform.LookAt(transform.position + shipVelocity, tempUp);
+       
+            shipVelocity -= (damping * shipVelocity * Time.deltaTime);
+        }
+        else
+        {
+            shipSpeed = 0;
+        }
+
+        
+    }
+}
--- Game Engines Game 2/Assets/Scripts/WayPoints.cs
+++ Game Engines Game 2/Assets/Scripts/WayPoints.cs
@@ -4,16 +4,60 @@ using UnityEngine;
 
 public class WayPoints : MonoBehaviour
 {
+    public List<Vector3> waypoints = new List<Vector3>();
 
+    public int wayPointNext = 0;
+    public bool loopedWaypoint = true;
+
+
+    // Use this for initialization
     void Start()
     {
-
+        waypoints.Clear();
+        int count = transform.childCount;
+        for (int i = 0; i < count; i++)
+        {
+            waypoints.Add(transform.GetChild(i).position);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public Vector3 NextPoint()
     {
+        return waypoints[wayPointNext];
+    }
 
+    public void MoveToNext()
+    {
+        if (loopedWaypoint)
+        {
+            wayPointNext = (wayPointNext + 1) % waypoints.Count;
+        }
+        else
+        {
+            if (wayPointNext != waypoints.Count - 1)
+            {
+                wayPointNext++;
+            }
+        }
+    }
+
+    public bool LastPoint()
+    {
+        return wayPointNext == waypoints.Count - 1;
+    }
+
+    public void OnDrawGizmos()
+    {
+        int count = loopedWaypoint ? (transform.childCount + 1) : transform.childCount;
+        Gizmos.color = Color.cyan;
+        for (int i = 1; i < count; i++)
+        {
+            Transform prevousPoint = transform.GetChild(i - 1);
+            Transform nextPoint = transform.GetChild(i % transform.childCount);
+            Gizmos.DrawLine(prevousPoint.transform.position, nextPoint.transform.position);
+            Gizmos.DrawSphere(prevousPoint.position, 1);
+            Gizmos.DrawSphere(nextPoint.position, 1);
+        }
     }
 
 }
