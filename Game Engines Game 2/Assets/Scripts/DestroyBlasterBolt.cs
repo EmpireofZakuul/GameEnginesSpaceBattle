2e3b2300b82eb91a61c1b5af08d9b40e69e051ef
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2021-03-10 19:20:55 +0000

    worked on firing bullets on the empires ship and the eternal fleet

---
+++ Game Engines Game 2/Assets/Scripts/DestroyBlasterBolt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBlasterBolt : MonoBehaviour
+{
+
+    public float destroy = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, destroy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Game Engines Game 2/Assets/Scripts/EmpireShipShooting.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmpireShipShooting : MonoBehaviour
+{
+    public float fireRate = 15f;
+    private float nextTimeToFire = 0f;
+    public GameObject projectile;
+    public Transform bulletSpawn;
+    //public GameObject[] spawnBullet;
+    public float speed = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)//testing 
+        {
+            nextTimeToFire = Time.time + 1 / fireRate;
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+       
+        GameObject bullet = Instantiate(projectile);
+       
+        bullet.transform.position = bulletSpawn.position;
+        Quaternion rotation = transform.rotation;
+        //bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * speed, ForceMode.Impulse);
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * speed, ForceMode.Impulse);
+
+
+    }
+}
