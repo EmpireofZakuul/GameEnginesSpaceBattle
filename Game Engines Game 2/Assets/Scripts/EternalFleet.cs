aaf1b1a81b9a7adec83b3e7e7336bb8062310358
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2021-03-17 19:42:53 +0000

    added ray casting to the eternal fleet

---
+++ Game Engines Game 2/Assets/Scripts/EternalFleet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EternalFleet : MonoBehaviour
+{
+    [Header("Ship Movment")]
+    public Vector3 shipVelocity;
+    public float shipSpeed;
+    public Vector3 shipAcceleration;
+    public Vector3 shipForce;
+    public float maxSpeed = 10;
+    public float maxForce = 20;
+
+    public float shipsMass = 1;
+
+    public float shipSlowingDistance = 80;
+
+    public float damping = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        shipAcceleration = shipForce / shipsMass;
+        shipVelocity = shipVelocity + shipAcceleration * Time.deltaTime;
+        transform.position = transform.position + shipVelocity * Time.deltaTime;
+        shipSpeed = shipVelocity.magnitude;
+        
+    }
+    
+
+}
---
+++ Game Engines Game 2/Assets/Scripts/EternalFleetFire.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EternalFleetFire : MonoBehaviour
+{
+    [Header("Ship Tracking")]
+    public Transform sensorRay;
+    public float RayDist = 10;
+    public bool targetFound = false;
+
+    [Header("Ship Firing")]
+    public float fireRate = 15f;
+    private float nextTimeToFire = 0f;
+    public GameObject projectile;
+    public Transform bulletSpawn;
+    public float speed = 20;
+
+
+    ObjectPool objectPooler;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectPooler = ObjectPool.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        Debug.DrawRay(this.transform.position, Vector3.left * 10000, Color.green);
+
+
+        if (Physics.Raycast(this.transform.position, Vector3.left , out hit, RayDist))
+        {
+            Debug.Log("casting ray");
+            
+            if (hit.collider.gameObject.tag == "Enemy")
+            {
+                Debug.Log(hit.point);
+                WeaponFirePosition();
+                targetFound = false;
+            }
+
+        }
+        if (!targetFound)
+        {
+            WeaponFirePosition();
+        }
+
+        if (targetFound)
+        {
+            nextTimeToFire += Time.deltaTime;
+            if (nextTimeToFire >= fireRate)//testing 
+            {
+                nextTimeToFire = 0;
+                Fire();
+            }
+        }
+        
+    }
+    public void WeaponFirePosition()
+    {
+
+    }
+    public void Fire()
+    {
+        objectPooler.SpawnFromPool("Bullet", bulletSpawn.position, bulletSpawn.rotation);
+    }
+}
