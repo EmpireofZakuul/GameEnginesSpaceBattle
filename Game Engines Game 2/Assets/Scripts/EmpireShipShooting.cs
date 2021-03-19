2cbdbfe95bdf99c0e54c04982257659e538e7ba8
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2021-03-19 19:20:30 +0000

    worked on eternal fleet firing, empire firing and sound effects

--- Game Engines Game 2/Assets/Scripts/EmpireShipShooting.cs
+++ Game Engines Game 2/Assets/Scripts/EmpireShipShooting.cs
@@ -4,57 +4,95 @@ using UnityEngine;
 
 public class EmpireShipShooting : MonoBehaviour
 {
+    [Header("Ship Tracking")]
+    public Transform sensorRay;
+    public float RayDist = 10;
+    public bool targetFound = false;
+    public bool GunsRotated = false;
+    public bool GunFiringLoop = false;
+    public RaycastHit hit;
+
+
+    [Header("Ship Firing")]
     public float fireRate = 15f;
     private float nextTimeToFire = 0f;
     public GameObject projectile;
     public Transform bulletSpawn;
     public float speed = 20;
+
+    [Header("Ship audio")]
+    public AudioSource AudioSource;
+    public AudioSource AudioSource2;
+    public bool audioOn = false;
+
+
     ObjectPool objectPooler;
-   
-    
 
-
+    // Start is called before the first frame update
     void Start()
     {
         objectPooler = ObjectPool.Instance;
+        AudioSource.GetComponent<AudioSource>();
+        AudioSource2.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        /* if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)//testing 
-         {
-             nextTimeToFire = Time.time + 1 / fireRate;
-             Shoot();
-         }
-        */
+       
 
-        nextTimeToFire += Time.deltaTime;
-        if (nextTimeToFire >= fireRate)//testing 
+        Debug.DrawRay(this.transform.position, Vector3.right * 9000, Color.red);
+
+
+        if (Physics.Raycast(this.transform.position, Vector3.right, out hit, RayDist))
         {
-            nextTimeToFire = 0;
-            Shoot();
+            Debug.Log("casting ray");
+
+            if (hit.collider.gameObject.tag == "EternalEnemy")
+            {
+                Debug.Log(hit.point);
+                targetFound = true;
+
+                if(targetFound && !audioOn )
+                {
+                    AudioSource2.Play();
+                    audioOn = true;
+                    
+                }
+                
+            }
+
         }
+        
+
+        if (targetFound && !GunFiringLoop)
+        {
+            nextTimeToFire += Time.deltaTime;
+            if (nextTimeToFire >= fireRate)//testing 
+            {
+                nextTimeToFire = 0;
+                StartCoroutine(Fire());
+
+            }
+        }
+
     }
 
-    public void Shoot()
+
+    public IEnumerator Fire()
     {
-        objectPooler.SpawnFromPool("Bullet", bulletSpawn.position, bulletSpawn.rotation);
-       
-    }
+        GunFiringLoop = true;
+        while (true)
+        {
+            GunFiringLoop = true;
+            objectPooler.SpawnFromPool("Bullet", bulletSpawn.position, bulletSpawn.rotation);
+            AudioSource.Play();
 
-    /*public void Shoot()
-    {
-       
-        GameObject bullet = Instantiate(projectile);
-       
-        bullet.transform.position = bulletSpawn.position;
-        Quaternion rotation = transform.rotation;
-        //bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * speed, ForceMode.Impulse);
-        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * speed, ForceMode.Impulse);
+            yield return new WaitForSeconds(1.5f);
 
+         
+        }
 
     }
-    */
 }
 
--- Game Engines Game 2/Assets/Scripts/EternalFleetFire.cs
+++ Game Engines Game 2/Assets/Scripts/EternalFleetFire.cs
@@ -7,8 +7,13 @@ public class EternalFleetFire : MonoBehaviour
 {
     [Header("Ship Tracking")]
     public Transform sensorRay;
+    public Transform gunPosition;
     public float RayDist = 10;
-    public bool targetFound = false;
+    public bool targetFound =  false;
+    public bool GunsRotated = false;
+    public bool GunFiringLoop = false;
+    public RaycastHit hit;
+
 
     [Header("Ship Firing")]
     public float fireRate = 15f;
@@ -27,12 +32,15 @@ public class EternalFleetFire : MonoBehaviour
     void Start()
     {
         objectPooler = ObjectPool.Instance;
+
+        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
+        //RaycastHit hit;
 
         Debug.DrawRay(this.transform.position, Vector3.left * 10000, Color.green);
 
@@ -44,25 +52,40 @@ public class EternalFleetFire : MonoBehaviour
             if (hit.collider.gameObject.tag == "Enemy")
             {
                 Debug.Log(hit.point);
-                targetFound = false;
-                //transform.Rotate.y = hit.point;
+                targetFound = true;
             }
 
         }
-        if (!targetFound)
+        if (targetFound )
         {
             //transform.rotation =  Quaternion.Euler(0, hit.point, 0);
-           //transform.localEulerAngles = new Vector3(0, hit.point, 0);
-            targetFound = true;
-        }
+            //transform.localEulerAngles = new Vector3(0, hit.point, 0);
 
-        if (targetFound)
+            //Vector3 targetDir = hit.point - gunPosition.position;
+           //transform.rotation =  Vector3.Angle(targetDir, Vector3.forward);
+
+            //transform.rotation = Vector3.Angle(gunPosition, hit.point);
+
+            // float angleToTarg = Vector3.Angle(transform.forward, gunPosition.transform.position - hit.point);
+            // transform.rotation = angleToTarg;
+            targetFound = true;
+            GunsRotated = true;
+        }
+        //if any ships raycast hits the target, all ships roatae there guns and fire
+
+       // else if ()
+       //{
+
+       //}
+
+        if (targetFound && GunsRotated && !GunFiringLoop)
         {
             nextTimeToFire += Time.deltaTime;
             if (nextTimeToFire >= fireRate)//testing 
             {
                 nextTimeToFire = 0;
                 StartCoroutine(Fire());
+               
             }
         }
         
@@ -71,20 +94,27 @@ public class EternalFleetFire : MonoBehaviour
 
     public IEnumerator Fire()
     {
-        objectPooler.SpawnFromPool("Bullet", bulletSpawn.position, bulletSpawn.rotation);
+        GunFiringLoop = true;
+        while (true)
+        {
+            GunFiringLoop = true;
+            objectPooler.SpawnFromPool("Bullet", bulletSpawn.position, bulletSpawn.rotation);
 
-        yield return new WaitForSeconds(1.5f);
+            yield return new WaitForSeconds(1.5f);
 
-        objectPooler.SpawnFromPool("Bullet", bulletSpawn2.position, bulletSpawn.rotation);
+            objectPooler.SpawnFromPool("Bullet", bulletSpawn2.position, bulletSpawn.rotation);
 
-        yield return new WaitForSeconds(1.5f);
+            yield return new WaitForSeconds(1.5f);
 
-        objectPooler.SpawnFromPool("Bullet", bulletSpawn3.position, bulletSpawn.rotation);
+            objectPooler.SpawnFromPool("Bullet", bulletSpawn3.position, bulletSpawn.rotation);
 
-        yield return new WaitForSeconds(1.5f);
+            yield return new WaitForSeconds(1.5f);
 
-        objectPooler.SpawnFromPool("Bullet", bulletSpawn4.position, bulletSpawn.rotation);
+            objectPooler.SpawnFromPool("Bullet", bulletSpawn4.position, bulletSpawn.rotation);
 
-        yield return new WaitForSeconds(1.5f);
+            yield return new WaitForSeconds(1.5f);
+        }
+       
     }
+   
 }
