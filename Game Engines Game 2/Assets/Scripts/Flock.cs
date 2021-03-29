1c68026ca05a7da4780a9ecd4175480f869392af
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2021-03-29 19:09:46 +0100

    still working on the flocking behaviour

--- Game Engines Game 2/Assets/Scripts/Flock.cs
+++ Game Engines Game 2/Assets/Scripts/Flock.cs
@@ -4,79 +4,89 @@ using UnityEngine;
 
 public class Flock : MonoBehaviour
 {
-     [Header("Flocking")]
+    [Header("Flocking")]
     public FlockAgent agentPrefab;
     List<FlockAgent> agents = new List<FlockAgent>();
     public FlockBehaviour behaviour;
 
     [Range(10,100)]
     public int shipStartCount = 40;
-    const float shipDensity = 0.0f;
+    [Range(0f, 10f)]
+    public float shipDensity = 0.1f;
     [Range(1f,100f)]
     public float driveFactor = 10f;
     [Range(1f,10f)]
     public float ShipMaxSpeed = 10;
+    [Range(1f, 10f)]
     public float neighborRadius = 1.5f; 
-     [Range(1f,1f)]
+    [Range(0f,1f)]
     public float ShipAdvidanceRadiusMultiplier = 0.5f; 
   
-    float neighborRadiusSquared;
-    float ShipAdvidanceRadiusSquared;
+    float _neighborRadiusSquared;
+    float _ShipAdvidanceRadiusSquared;
+    float _squaredMaxSpeed;
+    public float _squaredAdvidanceRadius { get { return _neighborRadiusSquared; } }
 
-
-    [Header("Spawning Ships")]
-    public int enemyAmoundMax = 10;
-    
-    public  int count = 0;
-    public  int totalcount = 0;
-    public GameObject[] spawnPoints;
-    private GameObject currentPoint;
-    private int EnemyIndex;
-    public bool spawning =  true;
-      ObjectPool objectPooler;
     // Start is called before the first frame update
     void Start()
     {
-        neighborRadiusSquared = neighborRadius * neighborRadius;
-        ShipAdvidanceRadiusSquared = neighborRadiusSquared * ShipAdvidanceRadiusMultiplier * ShipAdvidanceRadiusMultiplier;
+        _squaredMaxSpeed = ShipMaxSpeed * ShipMaxSpeed;
+        _neighborRadiusSquared = neighborRadius * neighborRadius;
+        _ShipAdvidanceRadiusSquared = _neighborRadiusSquared * ShipAdvidanceRadiusMultiplier * ShipAdvidanceRadiusMultiplier;
 
+        for (int i = 0; i < shipStartCount; i++)
+        {
+            FlockAgent shipAgent = Instantiate(
+                agentPrefab,
+                //transform.position,
+                Random.insideUnitCircle * shipStartCount * shipDensity,
+               transform.rotation,
+                transform
+                ) ;
 
+            shipAgent.name = "ShipAgent" + i;
+            agents.Add(shipAgent);
+        }
         
-        InvokeRepeating("SpawnEnemy", 2f,2f);
-         objectPooler = ObjectPool.Instance;
+       
     }
     
  
     // Update is called once per frame
       void Update()
-    {
+      { 
 
-
-
-        #region total amount
-        if(totalcount == 20)
+       foreach(FlockAgent agent in agents)
         {
-            spawning = false;
-        
+            List<Transform> context = GetNearByObjects(agent);
+
+            //Vector3 shipMove = behaviour.CalculateMove(agent, context, this);
+            //shipMove *= driveFactor;
+            //if(shipMove.sqrMagnitude > _squaredMaxSpeed)
+            //{
+            //    shipMove = move.normalized * ShipMaxSpeed;
+            //}
+            //agent.MoveShip(move);
         }
-        #endregion
-    }
-  
-    #region  spawning
-   public void SpawnEnemy()
-   {
-       if(totalcount < enemyAmoundMax && spawning == true)
-        {
-        
-        spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
-        EnemyIndex = Random.Range(0, spawnPoints.Length);
-        objectPooler.SpawnFromPool("Fighter", transform.position, transform.rotation);
-        currentPoint = spawnPoints[EnemyIndex];
-        totalcount++;
-        }return;
 
-   }
-   #endregion
+
+    }
+
+  
+   List<Transform> GetNearByObjects(FlockAgent agent)
+    {
+        List<Transform> context = new List<Transform>();
+        Collider[] colliders = Physics.OverlapSphere(agent.transform.position, neighborRadius);
+        foreach(Collider collider in colliders)
+        {
+            if(collider != agent.AgentCollider)
+            {
+                context.Add(collider.transform);
+            }
+        }
+        return context;
+    }
+       
 
 }
 
