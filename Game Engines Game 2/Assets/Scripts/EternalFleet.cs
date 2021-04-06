670b97597526580f65f7b98894da7288507d8646
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2021-04-06 18:13:59 +0100

    working on eternal fleet

--- Game Engines Game 2/Assets/Scripts/EternalFleet.cs
+++ Game Engines Game 2/Assets/Scripts/EternalFleet.cs
@@ -15,9 +15,10 @@ public class EternalFleet : MonoBehaviour
     //public float shipSlowingDistance = 80;
     //public float damping = 0.1f;
 
-   
 
 
+    public Transform cameraEnd;
+    public float speed = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -28,15 +29,16 @@ public class EternalFleet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    
+
         /*shipAcceleration = shipForce / shipsMass;
         shipVelocity = shipVelocity + shipAcceleration * Time.deltaTime;
         transform.position = transform.position + shipVelocity * Time.deltaTime;
         shipSpeed = shipVelocity.magnitude;
         */
 
-      
-        
+        transform.position = Vector3.MoveTowards(transform.position, cameraEnd.position, speed * Time.deltaTime);
+
+
     }
     public void ChangeMaterial()
     {
