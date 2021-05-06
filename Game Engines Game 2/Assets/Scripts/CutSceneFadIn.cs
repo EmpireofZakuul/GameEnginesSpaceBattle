ca04595420a37a6f705d1df8bf33cae5643f38d1
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2021-05-06 12:49:52 +0100

    title and cutscene done

---
+++ Game Engines Game 2/Assets/Scripts/CutSceneFadIn.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CutSceneFadIn : MonoBehaviour
+{
+    public Image fadIn;
+    public float timeRemaining = 44;
+    public bool timerIsRunning = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerIsRunning = true;
+        fadIn.canvasRenderer.SetAlpha(0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                StartCoroutine("Fade");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+        
+    }
+    IEnumerator Fade()
+    {
+        fadIn.CrossFadeAlpha(1, 5, false);
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ Game Engines Game 2/Assets/Scripts/TitleFadIn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleFadIn : MonoBehaviour
+{
+    public float timer = 10;
+    public float speed = 5f;
+    public Image fadIn;
+    public bool stopTimer = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadIn.canvasRenderer.SetAlpha(0f);
+        speed = GetComponentInParent<Canvas>().transform.lossyScale.y * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, speed * Time.deltaTime, 0);
+
+        timer -= Time.deltaTime;
+
+        if (timer <= 0 && !stopTimer)
+        {
+            StartCoroutine("Fade");
+            stopTimer = true;
+        }
+    }
+
+    IEnumerator Fade()
+    {
+        fadIn.CrossFadeAlpha(1, 5, false);
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(1);
+    }
+}
